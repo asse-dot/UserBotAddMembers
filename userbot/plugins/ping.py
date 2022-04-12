7b7c1bea724ec7b973976bdcca94646eaaf1479c
Christian677 <chryzen9@gmail.com>
2022-04-12 23:17:42 +0200

    fffff

---
+++ userbot/plugins/ping.py
@@ -0,0 +1,32 @@
+
+
+@UserBot.on_message(filters.command(["speed", "speedtest"], ".") & filters.me)
+async def speed_test(_, message: Message):
+    new_msg = await message.edit("`Running speed test . . .`")
+    spd = speedtest.Speedtest()
+
+    new_msg = await message.edit(
+        f"`{new_msg.text}`\n" "`Getting best server based on ping . . .`"
+    )
+    spd.get_best_server()
+
+    new_msg = await message.edit(f"`{new_msg.text}`\n" "`Testing download speed . . .`")
+    spd.download()
+
+    new_msg = await message.edit(f"`{new_msg.text}`\n" "`Testing upload speed . . .`")
+    spd.upload()
+
+    new_msg = await new_msg.edit(
+        f"`{new_msg.text}`\n" "`Getting results and preparing formatting . . .`"
+    )
+    results = spd.results.dict()
+
+    await message.edit(
+        WWW.SpeedTest.format(
+            start=results["timestamp"],
+            ping=results["ping"],
+            download=SpeedConvert(results["download"]),
+            upload=SpeedConvert(results["upload"]),
+            isp=results["client"]["isp"],
+        )
+    )
