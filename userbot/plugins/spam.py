c75918842d1e00289503a049a85f0a1367466c79
asse <86473751+asse-dot@users.noreply.github.com>
2022-04-12 22:10:38 +0200

    Update spam.py

--- userbot/plugins/spam.py
+++ userbot/plugins/spam.py
@@ -5,8 +5,11 @@ from pyrogram.types import Message
 from userbot import UserBot
 from userbot.helpers.PyroHelpers import ReplyCheck
 
+RUN = False
+
 @UserBot.on_message(filters.command("spam", ".") & filters.me)
 async def spam(_, message: Message):
+    global RUN
     # Get current chat and spam to there.
     # if in group and replied to user, then spam replying to user.
     
@@ -22,9 +25,12 @@ async def spam(_, message: Message):
     
     if not interval.isnumeric() and not times.isnumeric():
         return
-
+    RUN = True
     if message.chat.type in ["supergroup", "group"]:
         for _ in range(int(times)):
+           if not RUN:
+                break
+                
             await UserBot.send_message(
                 message.chat.id, to_spam, reply_to_message_id=ReplyCheck(message)
             )
@@ -32,5 +38,15 @@ async def spam(_, message: Message):
 
     if message.chat.type == "private":
         for _ in range(int(times)):
+            if not RUN:
+                break
+                
             await UserBot.send_message(message.chat.id, to_spam)
             await asyncio.sleep(int(interval))
+            
+    
+@UserBot.on_message(filters.command("stop", ".") & filters.me)
+async def stop(_, message: Message):
+    global RUN
+    if RUN:
+        RUN = False
